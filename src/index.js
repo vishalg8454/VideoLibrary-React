f4c0713082c6f4854e344faaecb290b491de85a7
Vishal Gaurav <lemeraemail@gmail.com>
2022-04-20 21:37:13 +0530

    initial commit: folder setup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
