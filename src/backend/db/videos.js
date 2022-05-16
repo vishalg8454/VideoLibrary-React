a3197cac543eb8c6fd4f7f5af9c8b39fa0913bec
Vishal Gaurav <lemeraemail@gmail.com>
2022-05-16 22:37:20 +0530

    feat: update video db

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,144 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+    _id: "xjnFYBw68WQ",
+    videoTitle: "Android @ I/O'22: A quick run through of what's new!",
+    channelName: "Android Developers",
+    description: `Here's a quick run through of the big pieces of news for Android developers at Google I/O!
+
+    From Jetpack Compose, Android Studio, Android 13 to tablets and Wear OS, it was a big year.
+    
+    We unpacked it all in another episode of #TheAndroidShow; you can watch the full show and playlist here → https://goo.gle/The-Android-Show
+    
+    Subscribe to Android Developers → https://goo.gle/AndroidDevs 
+    
+    #Featured #TheAndroidShow #GoogleIO`,
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/xjnFYBw68WQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBG-G_US1MhkHGRW9fHwl9-o6_TAA",
+    channelUrl:
+      "https://yt3.ggpht.com/OWAcq1Yu0stdx87oWaZNPMLWYhAyrGhmDTPyi13jlmfAGkkLO5RgYnLGoaXqG_ZOWXCiuqKk=s176-c-k-c0x00ffffff-no-rj",
+    viewCount: "202k views",
+    category: "Google IO",
+    publishedDate: "2days ago",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
-    description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+    _id: "CdrLp0lKjHc",
+    videoTitle: "Gravitas Plus: Which countries could go the Sri Lanka way",
+    channelName: "WION",
+    description: `Sri Lanka's debt crisis is a warning to the world. The World Bank says a global debt storm is coming. It could engulf 70 developing countries & make their economies fall like dominoes. What are the threats they face? How can they be averted? Palki Sharma explains.
+
+    #GravitasPlus #SriLanka #DebtCrisis 
+    
+    About Channel: 
+    
+    WION -The World is One News, examines global issues with in-depth analysis. We provide much more than the news of the day. Our aim to empower people to explore their world. With our Global headquarters in New Delhi, we bring you news on the hour, by the hour. We deliver information that is not biased. We are journalists who are neutral to the core and non-partisan when it comes to the politics of the world. People are tired of biased reportage and we stand for a globalised united world. So for us the World is truly One.
+     
+    Please keep discussions on this channel clean and respectful and refrain from using racist or sexist slurs as well as personal insults.`,
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/CdrLp0lKjHc/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCeSAVVR1dxRwe5YLyZKPUW1Jmtpg",
+    channelUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLQ5U-RWZZWrWBgS5imVahjSmIJpb21BW3Rwwd1XTw=s176-c-k-c0x00ffffff-no-rj",
+    viewCount: "2M views",
+    category: "News",
+    publishedDate: "7days ago",
+  },
+  {
+    _id: "YmhIp8V2p0Y",
+    videoTitle: "How to Take CS50 - CS50 Reels #Shorts",
+    channelName: "CS50",
+    description: `Professor David J. Malan explains how you can take CS50’s classes. Go to https://cs50.harvard.edu/x to see the upcoming Zoom classes, and watch the classes that have been recorded at https://youtube.com/cs50. Register officially at https://cs50.edx.org.
+
+    ***
+    
+    This is CS50, Harvard University's introduction to the intellectual enterprises of computer science and the art of programming.
+    
+    ***`,
+    thumbnailUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLTZloGQkLBOEhaQ7mR-DLvcslyP1bq4EIcIXPqNMA=s176-c-k-c0x00ffffff-no-rj",
+    channelUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLQ5U-RWZZWrWBgS5imVahjSmIJpb21BW3Rwwd1XTw=s176-c-k-c0x00ffffff-no-rj",
+    viewCount: "8601 views",
+    category: "Programming",
+    publishedDate: "1 month ago",
+  },
+  {
+    _id: "GGyML4ElQCc",
+    videoTitle: "[Audio Described] Google Keynote (Google I/O ‘22)",
+    channelName: "Google",
+    description: `Tune in to find out about how we're furthering our mission to organize the world’s information and make it universally accessible and useful.
+
+    To watch this keynote without audio descriptions, please click here:
+    https://youtu.be/nP-nMZpLM1A
+     
+    To watch this keynote with American Sign Language (ASL) interpretation, please click here: https://youtu.be/PeUXBvRExic
+    
+    Subscribe to our Channel: https://www.youtube.com/google
+    Tweet with us on Twitter: https://twitter.com/google
+    Follow us on Instagram: https://www.instagram.com/google
+    Join us on Facebook: https://www.facebook.com/Google`,
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/GGyML4ElQCc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCMLth4xLmmQwflFQ_mCvthZOJnrA",
+    channelUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLR14FbEI5TPYKnDB3-0cbOQ_HmI_gA0oozUtq2xyJM=s176-c-k-c0x00ffffff-no-rj",
+    viewCount: "700k views",
+    category: "Google IO",
+    publishedDate: "2days ago",
+  },
+  {
+    _id: "YFrDUmiL8yw",
+    videoTitle: "Top 15 Glassmorphism Effect Using HTML CSS & JS | Glassmorphism CSS | Glassmorphism Effect CSS",
+    channelName: "Animation Coding",
+    description: `Top Glassmorphism Effect UI Design HTML CSS JS . CSS Animation & Hover Effect . 
+    CSS Glassmorphism  Effect Hover Animation And Hover Effect - Glassmorphism 
+    
+    
+    
+    
+    
+    ============================== 
+    👇source code :free 
+    1-15 all here✅
+    01-02 :https://tei.ai/1obGI
+    03-08 :https://tei.ai/3KkYWnN
+    09       :https://youtu.be/E2O03zTX2BY
+    10       :https://youtu.be/hJQ_WL-w590
+    11-15 :https://tei.ai/DAreOOit9
+    
+    
+    ============================== `,
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/YFrDUmiL8yw/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDIZ3u1oAWtGylxszDp9ZKW9eE64Q",
+    channelUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLQqQTsj8wLYZxLORI6HQLBsaaB7gMzAkApCSUQhQw=s176-c-k-c0x00ffffff-no-rj",
+    viewCount: "1205 views",
+    category: "Programming",
+    publishedDate: "5 month ago",
+  },
+  {
+    _id: "hw3Bx5vxKl0",
+    videoTitle: "Solid in 100 Seconds",
+    channelName: "Fireship",
+    description: `SolidJS is a JavaScript framework for building fast, declarative UIs on the web. It shares many ideas with React, but does not use the virtual DOM to deliver a more performant and pragmatic developer experience.
+
+    #webdev #programming #100SecondsOfCode
+    
+    🔗 Resources
+    
+    Solid Docs https://www.solidjs.com/
+    Solid GitHub https://github.com/solidjs/solid
+    React in 100 Seconds https://youtu.be/Tn6-PIqc4UM
+    Svelte in 100 Seconds https://youtu.be/rv3Yq-B8qp4
+    
+    🔥 Get More Content - Upgrade to PRO
+    
+    Upgrade to Fireship PRO at https://fireship.io/pro
+    Use code lORhwXd2 for 25% off your first payment. `,
+    thumbnailUrl:
+      "https://i.ytimg.com/vi/hw3Bx5vxKl0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAt5howR5pJbDm7eXIFnbG9y7PpMA",
+    channelUrl:
+      "https://yt3.ggpht.com/ytc/AKedOLR5CDv14gL4DQ7I4gxIlBMY6u-CNsq2qfeev48R2g=s176-c-k-c0x00ffffff-no-rj",
+    viewCount: "45k views",
+    category: "Programming",
+    publishedDate: "1 hour ago",
   },
 ];
