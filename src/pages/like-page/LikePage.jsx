d627317b47a8b5962aee56093c4020956f38b40e
Vishal Gaurav <lemeraemail@gmail.com>
2022-06-07 01:04:42 +0530

    feat: implement Like Page

--- src/pages/like-page/LikePage.jsx
+++ src/pages/like-page/LikePage.jsx
@@ -1,14 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./LikePage.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchLikes } from "../../store/likeSlice";
+import { VideoCard } from "../../components";
 
 const LikePage = () => {
   const dispatch = useDispatch();
   const {
     user: { token },
   } = useSelector((store) => store.auth);
+  const { data: videosFromStore } = useSelector((state) => state.video);
   const { likes } = useSelector((store) => store.like);
+  const [videoList, setVideoList] = useState([]);
+
+  useEffect(() => {
+    const filteredVideos = videosFromStore.filter((video) =>
+      likes.some((it) => it._id === video._id)
+    );
+    setVideoList(filteredVideos);
+  }, [likes]);
 
   useEffect(() => {
     dispatch(fetchLikes({ token: token }));
@@ -18,6 +28,30 @@ const LikePage = () => {
       <p
         className={styles.likeCount}
       >{`You have ${likes.length} liked videos.`}</p>
+      <div className={styles.videoContainer}>
+        {videoList?.map(
+          ({
+            thumbnailUrl,
+            channelUrl,
+            videoTitle,
+            channelName,
+            viewCount,
+            publishedDate,
+            _id,
+          }) => (
+            <VideoCard
+              key={_id}
+              _id={_id}
+              thumbnailUrl={thumbnailUrl}
+              channelUrl={channelUrl}
+              videoTitle={videoTitle}
+              channelName={channelName}
+              viewCount={viewCount}
+              publishedDate={publishedDate}
+            />
+          )
+        )}
+      </div>
     </main>
   );
 };
