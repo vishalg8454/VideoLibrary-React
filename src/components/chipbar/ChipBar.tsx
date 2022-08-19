c6d3d73da732b94ab909baabab61872d7ada21e8
Vishal Gaurav <lemeraemail@gmail.com>
2022-08-19 11:37:25 +0530

    fix: fix scroll issue due to chipbar

--- src/components/chipbar/ChipBar.tsx
+++ src/components/chipbar/ChipBar.tsx
@@ -1,19 +1,19 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useRef } from "react";
 import styles from "./ChipBar.module.scss";
 
 type ChipProps = {
-  categoryName:string;
-  activeCategory:string;
-  onClickHandler:Dispatch<SetStateAction<string>>;
-}
+  categoryName: string;
+  activeCategory: string;
+  onClickHandler: Dispatch<SetStateAction<string>>;
+};
 
 type ChipBarProps = {
-  categories:string[];
-  activeCategory:string;
-  onClickHandler:Dispatch<SetStateAction<string>>;
-}
+  categories: string[];
+  activeCategory: string;
+  onClickHandler: Dispatch<SetStateAction<string>>;
+};
 
-const Chip = ({ categoryName, activeCategory, onClickHandler }:ChipProps) => {
+const Chip = ({ categoryName, activeCategory, onClickHandler }: ChipProps) => {
   return (
     <button
       className={
@@ -21,23 +21,30 @@ const Chip = ({ categoryName, activeCategory, onClickHandler }:ChipProps) => {
           ? styles.chipActive
           : styles.chipInActive
       }
-      onClick={()=>onClickHandler(categoryName)}
+      onClick={() => onClickHandler(categoryName)}
     >
       {categoryName}
     </button>
   );
 };
-const ChipBar = ({ categories , activeCategory, onClickHandler }:ChipBarProps) => {
+const ChipBar = ({
+  categories,
+  activeCategory,
+  onClickHandler,
+}: ChipBarProps) => {
+
   return (
-    <div className={styles.chipContainer}>
-      {categories.map((it) => (
-        <Chip
-          categoryName={it}
-          activeCategory={activeCategory}
-          key={it}
-          onClickHandler={onClickHandler}
-        />
-      ))}
+    <div data-foo="10px" className={styles.chipContainerWrapper}>
+      <div className={styles.chipContainer}>
+        {categories.map((it) => (
+          <Chip
+            categoryName={it}
+            activeCategory={activeCategory}
+            key={it}
+            onClickHandler={onClickHandler}
+          />
+        ))}
+      </div>
     </div>
   );
 };
